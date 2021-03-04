b82618691c85e6042d6da0f546ef73abe1b66aae
Tejaswaroop Vankadari <tvankadari@Tejaswaroops-MacBook-Pro.local>
2021-03-03 20:33:47 -0800

    changed header label

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return(
         <div className="header">
-            <h1>Welcome to Threat Information</h1>
+            <h1>Welcome to Sample Information</h1>
         </div>
     )
 };
